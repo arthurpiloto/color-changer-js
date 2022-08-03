d4fb2e01f9486be9394f7349abacb43d1931434d
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-03 10:11:33 -0300

    update do exercício

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 'use strict'
 
 function aplicarCor() {
-    const cor = document.getElementById('inputCor').value
+    const cor = document.getElementById('inputCor').value.toLowerCase()
+    if (cor == 'azul') {
+        document.documentElement.style.setProperty('--bg-color', '#476bd5')
+    } else if (cor == 'vermelho') {
+        document.documentElement.style.setProperty('--bg-color', '#d86755')
+    } else if (cor == 'verde') {
+        document.documentElement.style.setProperty('--bg-color', '#8cdd8c')
+    } else if (cor == 'amarelo') {
+        document.documentElement.style.setProperty('--bg-color', '#e6e67a')
+    } else {
+        alert('Digite uma das opções válidas')
+    }
+
     // document.body.style.backgroundColor = `#${cor}`
-    document.documentElement.style.setProperty('--bg-color', `#${cor}`)
+    // document.documentElement.style.setProperty('--bg-color', `#${cor}`)
+
 }
 
 document.getElementById('aplicarCor').addEventListener('click', aplicarCor)
